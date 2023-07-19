9dd2a0ae92ff2425ad404e62df377d5d78a3175a
TriLieu <lieuminhtri2003@gmail.com>
2023-07-19 11:28:46 +0700

    baibuoi6

---
+++ baitapb6/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace baitapb6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLuiHet = new System.Windows.Forms.Button();
+            this.btnLui1Trang = new System.Windows.Forms.Button();
+            this.btnToi1Trang = new System.Windows.Forms.Button();
+            this.btnToiHet = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMaSV = new System.Windows.Forms.TextBox();
+            this.txtTenSV = new System.Windows.Forms.TextBox();
+            this.txtDiemSV = new System.Windows.Forms.TextBox();
+            this.txtMaKhoa = new System.Windows.Forms.TextBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLuiHet
+            // 
+            this.btnLuiHet.Location = new System.Drawing.Point(51, 26);
+            this.btnLuiHet.Name = "btnLuiHet";
+            this.btnLuiHet.Size = new System.Drawing.Size(75, 23);
+            this.btnLuiHet.TabIndex = 0;
+            this.btnLuiHet.Text = "<";
+            this.btnLuiHet.UseVisualStyleBackColor = true;
+            this.btnLuiHet.Click += new System.EventHandler(this.btnLuiHet_Click);
+            // 
+            // btnLui1Trang
+            // 
+            this.btnLui1Trang.Location = new System.Drawing.Point(152, 26);
+            this.btnLui1Trang.Name = "btnLui1Trang";
+            this.btnLui1Trang.Size = new System.Drawing.Size(75, 23);
+            this.btnLui1Trang.TabIndex = 1;
+            this.btnLui1Trang.Text = "<<";
+            this.btnLui1Trang.UseVisualStyleBackColor = true;
+            this.btnLui1Trang.Click += new System.EventHandler(this.btnLui1Trang_Click);
+            // 
+            // btnToi1Trang
+            // 
+            this.btnToi1Trang.Location = new System.Drawing.Point(392, 26);
+            this.btnToi1Trang.Name = "btnToi1Trang";
+            this.btnToi1Trang.Size = new System.Drawing.Size(75, 23);
+            this.btnToi1Trang.TabIndex = 2;
+            this.btnToi1Trang.Text = ">>";
+            this.btnToi1Trang.UseVisualStyleBackColor = true;
+            this.btnToi1Trang.Click += new System.EventHandler(this.btnToi1Trang_Click);
+            // 
+            // btnToiHet
+            // 
+            this.btnToiHet.Location = new System.Drawing.Point(533, 26);
+            this.btnToiHet.Name = "btnToiHet";
+            this.btnToiHet.Size = new System.Drawing.Size(57, 23);
+            this.btnToiHet.TabIndex = 3;
+            this.btnToiHet.Text = ">";
+            this.btnToiHet.UseVisualStyleBackColor = true;
+            this.btnToiHet.Click += new System.EventHandler(this.btnToiHet_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(283, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Vị trí/thông số";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(140, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Mã SV";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(140, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Tên SV";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(140, 271);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Điểm SV";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(140, 338);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Mã Khoa";
+            // 
+            // txtMaSV
+            // 
+            this.txtMaSV.Location = new System.Drawing.Point(197, 140);
+            this.txtMaSV.Name = "txtMaSV";
+            this.txtMaSV.Size = new System.Drawing.Size(196, 20);
+            this.txtMaSV.TabIndex = 9;
+            // 
+            // txtTenSV
+            // 
+            this.txtTenSV.Location = new System.Drawing.Point(197, 206);
+            this.txtTenSV.Name = "txtTenSV";
+            this.txtTenSV.Size = new System.Drawing.Size(196, 20);
+            this.txtTenSV.TabIndex = 10;
+            // 
+            // txtDiemSV
+            // 
+            this.txtDiemSV.Location = new System.Drawing.Point(197, 271);
+            this.txtDiemSV.Name = "txtDiemSV";
+            this.txtDiemSV.Size = new System.Drawing.Size(196, 20);
+            this.txtDiemSV.TabIndex = 11;
+            // 
+            // txtMaKhoa
+            // 
+            this.txtMaKhoa.Location = new System.Drawing.Point(200, 338);
+            this.txtMaKhoa.Name = "txtMaKhoa";
+            this.txtMaKhoa.Size = new System.Drawing.Size(193, 20);
+            this.txtMaKhoa.TabIndex = 12;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(179, 433);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(58, 23);
+            this.btnThem.TabIndex = 13;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(271, 433);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(58, 23);
+            this.btnLuu.TabIndex = 14;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(362, 433);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(58, 23);
+            this.btnXoa.TabIndex = 15;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 563);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtMaKhoa);
+            this.Controls.Add(this.txtDiemSV);
+            this.Controls.Add(this.txtTenSV);
+            this.Controls.Add(this.txtMaSV);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnToiHet);
+            this.Controls.Add(this.btnToi1Trang);
+            this.Controls.Add(this.btnLui1Trang);
+            this.Controls.Add(this.btnLuiHet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLuiHet;
+        private System.Windows.Forms.Button btnLui1Trang;
+        private System.Windows.Forms.Button btnToi1Trang;
+        private System.Windows.Forms.Button btnToiHet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMaSV;
+        private System.Windows.Forms.TextBox txtTenSV;
+        private System.Windows.Forms.TextBox txtDiemSV;
+        private System.Windows.Forms.TextBox txtMaKhoa;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnXoa;
+    }
+}
+
---
+++ baitapb6/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace baitapb6
+{
+    public partial class Form1 : Form
+    {
+        String strcnn = "Server=TRI-LIEU\\SQLEXPRESS;Database=QLSV;Trusted_Connection=True;";
+        BindingSource bs;
+        DataSet ds;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            using (SqlConnection cnn = new SqlConnection(strcnn))
+            {
+                cnn.Open();
+                SqlDataAdapter adt = new SqlDataAdapter("SELECT * FROM SinhVien", cnn);
+                SqlCommandBuilder builder = new SqlCommandBuilder(adt);
+                ds = new DataSet();
+                adt.Fill(ds, "SinhVien");
+                bs = new BindingSource(ds, "SinhVien");
+                txtMaSV.DataBindings.Add("Text", bs, "MaSV");
+                txtTenSV.DataBindings.Add("Text", bs, "TenSV");
+                txtDiemSV.DataBindings.Add("Text", bs, "DiemSV");
+                txtMaKhoa.DataBindings.Add("Text", bs, "MaKhoa");
+            }
+        }
+
+        private void btnLuiHet_Click(object sender, EventArgs e)
+        {
+            if (bs != null && bs.Position > 0)
+            {
+                bs.MoveFirst();
+            }
+        }
+
+        private void btnLui1Trang_Click(object sender, EventArgs e)
+        {
+            if (bs != null && bs.Position < bs.Count - 1)
+            {
+                bs.Position += 1;
+            }
+        }
+
+        private void btnToi1Trang_Click(object sender, EventArgs e)
+        {
+            if (bs != null && bs.Position > 0)
+            {
+                bs.Position -= 1;
+            }
+        }
+
+        private void btnToiHet_Click(object sender, EventArgs e)
+        {
+            if (bs != null && bs.Position < bs.Count - 1)
+            {
+                bs.MoveLast();
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            if (bs == null)
+            {
+                bs = new BindingSource();
+                ds = new DataSet();
+                ds.Tables.Add("SinhVien");
+                ds.Tables["SinhVien"].Columns.Add("MaSV");
+                ds.Tables["SinhVien"].Columns.Add("TenSV");
+                ds.Tables["SinhVien"].Columns.Add("DiemSV");
+                ds.Tables["SinhVien"].Columns.Add("MaKhoa");
+                ds.Tables["SinhVien"].PrimaryKey = new DataColumn[] { ds.Tables["SinhVien"].Columns["MaSV"] };
+                bs.DataSource = ds.Tables["SinhVien"];
+                txtMaSV.DataBindings.Add("Text", bs, "MaSV");
+                txtTenSV.DataBindings.Add("Text", bs, "TenSV");
+                txtDiemSV.DataBindings.Add("Text", bs, "DiemSV");
+                txtMaKhoa.DataBindings.Add("Text", bs, "MaKhoa");
+            }
+            bs.AddNew();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (bs != null && bs.Position >= 0)
+            {
+                using (SqlConnection cnn = new SqlConnection(strcnn))
+                {
+                    cnn.Open();
+                    SqlTransaction transaction = cnn.BeginTransaction();
+                    try
+                    {
+                        // Delete related records from KetQua table
+                        SqlCommand cmd = new SqlCommand("DELETE FROM KetQua WHERE MaSV = @MaSV", cnn, transaction);
+                        cmd.Parameters.AddWithValue("@MaSV", txtMaSV.Text);
+                        cmd.ExecuteNonQuery();
+
+                        // Delete record from SinhVien table
+                        bs.RemoveCurrent();
+                        SqlDataAdapter adt = new SqlDataAdapter("SELECT * FROM SinhVien", cnn);
+                        SqlCommandBuilder builder = new SqlCommandBuilder(adt);
+                        adt.Update(ds.Tables["SinhVien"]);
+
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            if (bs != null)
+            {
+                bs.EndEdit();
+                using (SqlConnection cnn = new SqlConnection(strcnn))
+                {
+                    cnn.Open();
+
+                    // Delete related records from KetQua table
+                    SqlCommand cmd = new SqlCommand("DELETE FROM KetQua WHERE MaSV = @MaSV", cnn);
+                    cmd.Parameters.AddWithValue("@MaSV", txtMaSV.Text);
+                    cmd.ExecuteNonQuery();
+
+                    SqlDataAdapter adt = new SqlDataAdapter("SELECT * FROM SinhVien", cnn);
+                    SqlCommandBuilder builder = new SqlCommandBuilder(adt);
+                    adt.Update(ds.Tables["SinhVien"]);
+                }
+            }
+        }
+    }
+}
